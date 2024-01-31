04e4258d331ee6b383f6d8420c3e534094d5050d
“skr111” <526206383@qq.com>
2024-01-31 10:37:16 +0800

    first commit

---
+++ src/LayOut/SideBar.tsx
@@ -0,0 +1,12 @@
+const SideBar = () => {
+  return (
+    <div>
+      <div>
+        <div>ADMINS</div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default SideBar
---
+++ src/LayOut/config.d.ts
@@ -0,0 +1,4 @@
+declare module './LayOut/SideBar' {
+  const SideBar: React.FC<any>;
+  export default SideBar;
+}
